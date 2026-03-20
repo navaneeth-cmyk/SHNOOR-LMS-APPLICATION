63be652e370160595828d0c8bb20523256d14ddf
Navaneeth <navaneethbharadwaj@gmail.com>
2026-03-20 16:24:38 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,4 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  resolve: {
+    alias: {
+      '@auth': '/src/auth',
+      '@context': '/src/context',
+    },
+  },
 })
